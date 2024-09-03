c8de58d46acd4d201b05fad4ea92072918833e96
jimmyxuexue <1002661758@qq.com>
2024-09-03 19:10:03 +0800

    增加打包ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
@@ -13,26 +14,36 @@ export default defineConfig({
 			},
 			{
 				find: '@/utils',
+				// @ts-ignore
 				replacement: resolve(__dirname, 'src/utils'),
 			},
 			{
 				find: '@/hooks',
+				// @ts-ignore
 				replacement: resolve(__dirname, 'src/hooks'),
 			},
 			{
 				find: '@/components',
+				// @ts-ignore
+
 				replacement: resolve(__dirname, 'src/components'),
 			},
 			{
 				find: '@/api',
+				// @ts-ignore
+
 				replacement: resolve(__dirname, 'src/api'),
 			},
 			{
 				find: '@/pages',
+				// @ts-ignore
+
 				replacement: resolve(__dirname, 'src/pages'),
 			},
 			{
 				find: '@/assets',
+				// @ts-ignore
+
 				replacement: resolve(__dirname, 'src/assets'),
 			},
 		],
