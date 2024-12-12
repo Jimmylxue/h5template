7f80af366abb004566e5cc65597b402e4ccc19fe
jimmyxuexue <1002661758@qq.com>
2024-12-12 10:24:59 +0800

    商品点击toast更新

--- src/pages/luckDraw/view/OtherProduct.tsx
+++ src/pages/luckDraw/view/OtherProduct.tsx
@@ -15,11 +15,16 @@ import {
 import Tag2 from '@/assets/img/common/Tag2.png'
 import classNames from 'classnames'
 import { useTranslation } from 'react-i18next'
+import { Toast } from 'react-vant'
 
 const lang = import.meta.env.VITE_APP_LANGUAGE as 'zh' | 'en' | 'xjp' | 'tai'
 
 export function OtherProduct() {
 	const { t } = useTranslation()
+
+	const jumpApp = () => {
+		Toast.info(t('luck2.jumpApp'))
+	}
 
 	return (
 		<div>
@@ -29,12 +34,28 @@ export function OtherProduct() {
 						src={other1}
 						className="w-[315px] h-[78px] bg-red-200 mx-auto mt-2"
 						alt=""
+						onClick={jumpApp}
 					/>
 
 					<div className=" flex justify-center pr-4 mt-6 ">
-						<img src={other2_1} className="w-[109px]" alt="" />
-						<img src={other2_2} className="w-[109px]" alt="" />
-						<img src={other2_3} className="w-[109px]" alt="" />
+						<img
+							src={other2_1}
+							className="w-[109px]"
+							alt=""
+							onClick={jumpApp}
+						/>
+						<img
+							src={other2_2}
+							className="w-[109px]"
+							alt=""
+							onClick={jumpApp}
+						/>
+						<img
+							src={other2_3}
+							className="w-[109px]"
+							alt=""
+							onClick={jumpApp}
+						/>
 					</div>
 					{/* <div className="w-[315px] h-[162px] bg-red-200 mx-auto mt-5"></div> */}
 
@@ -44,22 +65,48 @@ export function OtherProduct() {
 						src={other4}
 						className=" absolute left-1/2 -translate-x-1/2 bottom-2 w-[96px]"
 						alt=""
+						onClick={jumpApp}
 					/>
 				</div>
 			</div>
 
 			<div className=" px-4">
 				<div className=" bg-[url('/src/assets/taiImg/luck2/other5_bg.png')] h-[139px] w-full bg-cover mt-4 flex justify-between px-1 pt-10">
-					<img src={other5_1} className="w-[163px] h-[87px]" alt="" />
-					<img src={other5_2} className="w-[163px] h-[87px]" alt="" />
+					<img
+						src={other5_1}
+						className="w-[163px] h-[87px]"
+						alt=""
+						onClick={jumpApp}
+					/>
+					<img
+						src={other5_2}
+						className="w-[163px] h-[87px]"
+						alt=""
+						onClick={jumpApp}
+					/>
 				</div>
 			</div>
 
 			<div className=" flex justify-between px-4 mt-2">
-				<img src={other6_1} className=" w-[168px] h-[234px]" alt="" />
+				<img
+					src={other6_1}
+					onClick={jumpApp}
+					className=" w-[168px] h-[234px]"
+					alt=""
+				/>
 				<div className=" flex flex-col justify-between">
-					<img src={other6_2} className=" w-[168px] h-[113px]" alt="" />
-					<img src={other6_3} className=" w-[168px] h-[113px]" alt="" />
+					<img
+						src={other6_2}
+						onClick={jumpApp}
+						className=" w-[168px] h-[113px]"
+						alt=""
+					/>
+					<img
+						src={other6_3}
+						onClick={jumpApp}
+						className=" w-[168px] h-[113px]"
+						alt=""
+					/>
 				</div>
 			</div>
 
@@ -93,6 +140,7 @@ export function OtherProduct() {
 						src={other7_1}
 						className=" w-[308px] h-[242px] mx-auto mt-4"
 						alt=""
+						onClick={jumpApp}
 					/>
 				</div>
 			</div>
