be9acc7ca7831b2ef3bc0a84966c1b52175b0e38
jimmyxuexue <1002661758@qq.com>
2024-12-14 18:50:57 +0800

    luck泰语更新

--- src/pages/luckDraw/view/LuckAbout.tsx
+++ src/pages/luckDraw/view/LuckAbout.tsx
@@ -1,15 +1,153 @@
-import { com1, com2, com3, com4, com5, com6 } from '@/assets/luck2'
+import comTitle from '@/assets/img/common/comTitle.png'
+import otherTag from '@/assets/img/common/otherTag.png'
+import avatar1 from '@/assets/img/common/avatar1.png'
+import avatar2 from '@/assets/img/common/avatar2.png'
+import { useTranslation } from 'react-i18next'
 
 export function LuckAbout() {
+	const { t } = useTranslation()
 	return (
 		<div className="px-4">
-			<div className=" bg-[url('/src/assets/img/common/bigBg.png')] h-[1750px] w-full bg-cover pt-[80px]">
-				<img src={com1} className=" w-[317px] mx-auto" alt="" />
-				<img src={com2} className=" w-[317px] mx-auto" alt="" />
-				<img src={com3} className=" w-[317px] mx-auto mt-6" alt="" />
-				<img src={com4} className=" w-[317px] mx-auto mt-6" alt="" />
-				<img src={com5} className=" w-[317px] mx-auto mt-6" alt="" />
-				<img src={com6} className=" w-[317px] mx-auto" alt="" />
+			<div className="  w-full bg-gradient-to-b from-[#f7ded3] to-[#eea6f7] pt-[80px] px-4 pb-6">
+				<div className=" bg-[#faeaee] w-full py-4 rounded-xl bg-gradient-to-tr from-[#fbeaef] to-[#eee4fb]">
+					<div className=" flex items-start">
+						<div className=" size-[8px] bg-[#B69EFA] rounded-full flex-shrink-0 mt-2 mx-2"></div>
+						<div className=" text-sm">{t('luck2.text1')}</div>
+					</div>
+
+					<div className=" flex items-start mt-4">
+						<div className=" size-[8px] bg-[#B69EFA] rounded-full flex-shrink-0 mt-2 mx-2"></div>
+						<div className=" text-sm">{t('luck2.text2')}</div>
+					</div>
+				</div>
+
+				<div className=" w-full bg-white px-2 pt-10 pb-4 mt-8 relative">
+					<div className=" w-[150px] h-[40px] absolute left-1/2 -translate-x-1/2 -top-[10px] flex justify-center items-center">
+						<img src={comTitle} alt="" className=" absolute" />
+						<div className=" absolute text-white text-lg">
+							{t('luck2.goodIntroTitle')}
+						</div>
+					</div>
+
+					<div className=" size-[80px] absolute -top-6">
+						<img src={otherTag} className=" w-full h-full" alt="" />
+					</div>
+
+					<div className="bg-[#fef7fe] py-3 flex justify-center items-center relative mt-8 text-sm">
+						<div>{t('luck2.goodIntroSubDetail1')}</div>
+						<div className=" absolute text-white px-4 py-1 rounded-2xl bg-[#ca8fe8] -top-6">
+							{t('luck2.goodIntroSubTitle1')}
+						</div>
+					</div>
+
+					<div className="bg-[#fef7fe] py-3 flex justify-center items-center relative px-2 mt-10 text-sm">
+						<div>{t('luck2.goodIntroSubDetail2')}</div>
+						<div className=" absolute text-white px-4 py-1 rounded-2xl bg-[#ca8fe8] -top-6">
+							{t('luck2.goodIntroSubTitle2')}
+						</div>
+					</div>
+				</div>
+
+				<div className=" w-full bg-white px-2 text-sm pt-10 pb-2 relative mt-8">
+					<div className=" w-[150px] h-[40px] absolute left-1/2 -translate-x-1/2 -top-[10px] flex justify-center items-center">
+						<img src={comTitle} alt="" className=" absolute" />
+						<div className=" absolute text-white text-lg">
+							{t('luck2.ruleTitle')}
+						</div>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3">
+						<span className=" text-[#825FD2]">{t('luck2.ruleColorText1')}</span>
+						<span>{t('luck2.ruleText1')}</span>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3 mt-4">
+						<span className=" text-[#825FD2]">{t('luck2.ruleColorText2')}</span>
+						<span>{t('luck2.ruleText2')}</span>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3 mt-4">
+						<span className=" text-[#825FD2]">{t('luck2.ruleColorText3')}</span>
+						<span>{t('luck2.ruleText3')}</span>
+					</div>
+
+					<div className=" px-2 mt-4 mb-2">{t('luck2.ruleOther')}</div>
+				</div>
+
+				<div className=" w-full bg-gradient-to-br from-[#f7e1d5] to-[#fffefe] rounded-2xl pt-2 pb-4 mt-6">
+					<div className=" flex justify-center relative">
+						<div className=" relative z-10">{t('luck2.payTitle')}</div>
+						<div className=" absolute bg-[#f09d4a] w-28 h-2 bottom-[1px] rounded-lg"></div>
+					</div>
+
+					<div className=" flex px-4 text-sm mt-2">
+						<div className=" size-[8px] rounded-full bg-[#b29ff4] flex-shrink-0 mt-2 mr-2"></div>
+						<div>
+							<span className=" text-[#825FD2]">
+								{t('luck2.payColorText1')}
+							</span>
+							<span>{t('luck2.payText1')}</span>
+						</div>
+					</div>
+
+					<div className=" flex px-4 text-sm mt-4">
+						<div className=" size-[8px] rounded-full bg-[#b29ff4] flex-shrink-0 mt-2 mr-2"></div>
+						<div>
+							<span className=" text-[#825FD2]">
+								{t('luck2.payColorText2')}
+							</span>
+							<span>{t('luck2.payText2')}</span>
+						</div>
+					</div>
+				</div>
+
+				<div className=" w-full bg-white mt-8 px-2 text-sm pt-10 pb-4 relative">
+					<div className=" w-[150px] h-[40px] absolute left-1/2 -translate-x-1/2 -top-[10px] flex justify-center items-center">
+						<img src={comTitle} alt="" className=" absolute" />
+						<div className=" absolute text-white text-lg">
+							{t('luck2.noticeTitle')}
+						</div>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3">
+						<span>{t('luck2.noticeText1')}</span>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3 mt-4">
+						<span>{t('luck2.noticeText2')}</span>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3 mt-4">
+						<span>{t('luck2.noticeText3')}</span>
+					</div>
+				</div>
+
+				<div className=" w-full bg-white mt-4 text-sm px-2 pt-8 pb-4 relative">
+					<div className=" size-[80px] absolute -top-6 right-2">
+						<img src={otherTag} className=" w-full h-full" alt="" />
+					</div>
+
+					<div className=" flex justify-center relative">
+						<div className=" relative z-10">{t('luck2.userTitle')}</div>
+						<div className=" absolute bg-[#cfb9f7] w-28 h-2 bottom-[1px] rounded-xl"></div>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3 mt-2">
+						<div className=" flex items-center">
+							<img src={avatar1} className=" size-[40px] rounded-full" alt="" />
+							<div className=" text-lg ml-2">{t('luck2.user1Name')}</div>
+						</div>
+						<div className=" mt-2">{t('luck2.userText1')}</div>
+					</div>
+
+					<div className=" bg-[#fef7fe] px-2 py-3 mt-2">
+						<div className=" flex items-center">
+							<img src={avatar2} className=" size-[40px] rounded-full" alt="" />
+							<div className=" text-lg ml-2">{t('luck2.user2Name')}</div>
+						</div>
+						<div className=" mt-2">{t('luck2.userText2')}</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
--- src/pages/luckDraw/view/OtherProduct.tsx
+++ src/pages/luckDraw/view/OtherProduct.tsx
@@ -1,23 +1,17 @@
-import {
-	other1,
-	other2_1,
-	other2_2,
-	other2_3,
-	other3,
-	other4,
-	other5_1,
-	other5_2,
-	other6_1,
-	other6_2,
-	other6_3,
-	other7_1,
-} from '@/assets/luck2'
-import Tag2 from '@/assets/img/common/Tag2.png'
-import classNames from 'classnames'
 import { useTranslation } from 'react-i18next'
 import { Toast } from 'react-vant'
-
-const lang = import.meta.env.VITE_APP_LANGUAGE as 'zh' | 'en' | 'xjp' | 'tai'
+import pod1 from '@/assets/img/common/pod1.png'
+import pod2 from '@/assets/img/common/pod2.png'
+import pod3 from '@/assets/img/common/pod3.png'
+import pod4 from '@/assets/img/common/pod4.png'
+import pod5 from '@/assets/img/common/pod5.png'
+import pod6 from '@/assets/img/common/pod6.png'
+import pod7 from '@/assets/img/common/pod7.png'
+import pod8 from '@/assets/img/common/pod8.png'
+import pod9 from '@/assets/img/common/pod9.png'
+import pod10 from '@/assets/img/common/pod10.png'
+import otherTag from '@/assets/img/common/otherTag.png'
+import { disPrice1, disPrice2, disPrice3 } from '@/assets/luck2'
 
 export function OtherProduct() {
 	const { t } = useTranslation()
@@ -29,103 +23,240 @@ export function OtherProduct() {
 	return (
 		<div>
 			<div className="px-4">
-				<div className=" bg-[url('/src/assets/img/common/smallBg.png')] h-[460px] w-full bg-cover pt-[80px] relative z-1">
-					<img
-						src={other1}
-						className="w-[315px] h-[78px] bg-red-200 mx-auto mt-2"
-						alt=""
-						onClick={jumpApp}
-					/>
-
-					<div className=" flex justify-center pr-4 mt-6 ">
-						<img
-							src={other2_1}
-							className="w-[109px]"
-							alt=""
+				<div className="px-4 w-full pt-[80px] relative z-1 bg-gradient-to-br from-[#f7ded4] to-[#eea6f7] rounded-2xl pb-4">
+					<div className=" w-full bg-[#fae4c2] rounded-2xl h-[80px] overflow-hidden flex ">
+						<div className=" w-3/4 bg-[#fef9f9] h-full rounded-tr-[45px] flex justify-between items-center px-2">
+							<div className=" relative flex flex-col justify-center items-center ">
+								<div className="text-xs absolute -top-2 left-1">
+									{t('luck2.priceUnit')}
+								</div>
+								<div className=" text-3xl font-semibold">
+									{t('luck2.coupon1Price')}
+								</div>
+								<div className=" text-xs text-center leading-3">
+									{t('luck2.coupon1Desc')}
+								</div>
+							</div>
+							<div className=" w-[2px] h-8 bg-[#fae4c2]"></div>
+							<div className=" relative flex flex-col justify-center items-center ">
+								<div className="text-xs absolute -top-2 left-1">
+									{t('luck2.priceUnit')}
+								</div>
+								<div className=" text-3xl font-semibold">
+									{t('luck2.coupon2Price')}
+								</div>
+								<div className=" text-xs text-center leading-3">
+									{t('luck2.coupon2Desc')}
+								</div>
+							</div>
+							<div className=" w-[2px] h-8 bg-[#fae4c2]"></div>
+							<div className=" relative flex flex-col justify-center items-center ">
+								<div className="text-xs absolute -top-2 left-1">
+									{t('luck2.priceUnit')}
+								</div>
+								<div className=" text-3xl font-semibold">
+									{t('luck2.coupon3Price')}
+								</div>
+								<div className=" text-xs text-center leading-3">
+									{t('luck2.coupon3Desc')}
+								</div>
+							</div>
+						</div>
+						<div
+							className=" w-1/4 text-[#947947] pl-4 pr-4 flex justify-center items-center"
 							onClick={jumpApp}
-						/>
-						<img
-							src={other2_2}
-							className="w-[109px]"
-							alt=""
-							onClick={jumpApp}
-						/>
-						<img
-							src={other2_3}
-							className="w-[109px]"
-							alt=""
-							onClick={jumpApp}
-						/>
+						>
+							{t('luck2.addVip')}
+						</div>
 					</div>
-					{/* <div className="w-[315px] h-[162px] bg-red-200 mx-auto mt-5"></div> */}
 
-					<img src={other3} className=" w-[315px] mx-auto mt-4" alt="" />
+					<div className=" flex justify-between mt-4">
+						<div
+							className=" w-[95px] h-[160px] bg-[#fef9f9] rounded-lg text-xs flex flex-col justify-center items-center pt-2 relative"
+							onClick={jumpApp}
+						>
+							<div className=" text-center font-semibold">
+								{t('luck2.prod1Name')}
+							</div>
+							<div className=" flex justify-center mt-2">
+								<img className=" w-[70%]" src={pod1} alt="" />
+							</div>
+							<div className=" text-center text-[10px] text-[#9a8051] line-through">
+								{t('luck2.prod1Origin')}
+							</div>
+							<div className=" flex justify-center items-center">
+								<div className=" mt-2 text-[10px]">
+									{t('luck2.nowPriceText')}
+								</div>
+								<div className=" text-2xl font-light">
+									{t('luck2.prod1Now')}
+								</div>
+							</div>
+							<img
+								src={disPrice1}
+								className=" absolute size-[38px] -left-4 top-4"
+								alt=""
+							/>
+						</div>
 
-					<img
-						src={other4}
-						className=" absolute left-1/2 -translate-x-1/2 bottom-2 w-[96px]"
-						alt=""
-						onClick={jumpApp}
-					/>
+						<div
+							className=" w-[95px] h-[160px] bg-[#fef9f9] rounded-lg text-xs flex flex-col justify-center items-center pt-2 relative"
+							onClick={jumpApp}
+						>
+							<div className=" text-center font-semibold">
+								{t('luck2.prod2Name')}
+							</div>
+							<div className=" flex justify-center mt-2">
+								<img className=" w-[55%]" src={pod2} alt="" />
+							</div>
+							<div className=" text-center text-[10px] text-[#9a8051] line-through">
+								{t('luck2.prod2Origin')}
+							</div>
+							<div className=" flex justify-center items-center">
+								<div className=" mt-2 text-[10px]">
+									{t('luck2.nowPriceText')}
+								</div>
+								<div className=" text-2xl font-light">
+									{t('luck2.prod2Now')}
+								</div>
+							</div>
+							<img
+								src={disPrice2}
+								className=" absolute size-[38px] -left-4 top-4"
+								alt=""
+							/>
+						</div>
+
+						<div
+							className=" w-[95px] h-[160px] bg-[#fef9f9] rounded-lg text-xs flex flex-col justify-center items-center pt-2 relative"
+							onClick={jumpApp}
+						>
+							<div className=" text-center font-semibold">
+								{t('luck2.prod3Name')}
+							</div>
+							<div className=" flex justify-center mt-2">
+								<img className=" w-[70%]" src={pod3} alt="" />
+							</div>
+							<div className=" text-center text-[10px] text-[#9a8051] line-through">
+								{t('luck2.prod3Origin')}
+							</div>
+							<div className=" flex justify-center items-center">
+								<div className=" mt-2 text-[10px]">
+									{t('luck2.nowPriceText')}
+								</div>
+								<div className=" text-2xl font-light">
+									{t('luck2.prod3Now')}
+								</div>
+							</div>
+							<img
+								src={disPrice3}
+								className=" absolute size-[38px] -left-4 top-4"
+								alt=""
+							/>
+						</div>
+					</div>
+
+					<div className=" w-full h-[80px] bg-gradient-to-br from-[#fae9ee] to-[#ede3fb] rounded-xl flex items-center justify-between px-2 mt-4 relative">
+						<img src={pod4} className=" w-[45%] flex-shrink-0" alt="" />
+						<div className=" text-center px-2 font-semibold flex flex-col items-center">
+							{t('luck2.gainProductText')}
+							<div
+								className=" text-[10px] bg-[#f9d9b6] text-[#88552e] flex w-fit px-4 py-1 rounded-2xl"
+								onClick={jumpApp}
+							>
+								{t('luck2.gainBtnText')}
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
 			<div className=" px-4">
-				<div className=" bg-[url('/src/assets/taiImg/luck2/other5_bg.png')] h-[139px] w-full bg-cover mt-4 flex justify-between px-1 pt-10">
-					<img
-						src={other5_1}
-						className="w-[163px] h-[87px]"
-						alt=""
-						onClick={jumpApp}
-					/>
-					<img
-						src={other5_2}
-						className="w-[163px] h-[87px]"
-						alt=""
-						onClick={jumpApp}
-					/>
+				<div className=" w-full bg-gradient-to-b from-[#f7e0d5] to-[#ddcdf9] rounded-2xl mt-4 px-2 pt-2">
+					<div>{t('luck2.smartUse')}</div>
+
+					<div className=" text-sm flex justify-between">
+						<div
+							className=" flex bg-white rounded-2xl justify-between px-2 w-[49%] py-4"
+							onClick={jumpApp}
+						>
+							<img src={pod5} className="  w-[45%] flex flex-shrink-0" alt="" />
+							<div className=" flex flex-col justify-center items-center">
+								<div className=" text-2xl text-[#9f3cbb]">
+									{t('luck2.smartUse1Price')}
+								</div>
+								<div className=" text-xs">{t('luck2.smartUse1Text')}</div>
+							</div>
+						</div>
+						<div
+							className=" flex bg-white rounded-2xl justify-between px-2 w-[49%] py-4 "
+							onClick={jumpApp}
+						>
+							<img src={pod6} className="  w-[45%] flex flex-shrink-0" alt="" />
+							<div className=" flex flex-col justify-center items-center">
+								<div className=" text-2xl text-[#9f3cbb]">
+									{t('luck2.smartUse2Price')}
+								</div>
+								<div className=" text-xs">{t('luck2.smartUse2Text')}</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
-			<div className=" flex justify-between px-4 mt-2">
-				<img
-					src={other6_1}
+			<div className=" px-4 text-sm flex justify-between mt-4">
+				<div
+					className=" bg-gradient-to-br from-[#d5d5f9] to-[#fefeff] w-[48%] p-3 rounded-2xl"
 					onClick={jumpApp}
-					className=" w-[168px] h-[234px]"
-					alt=""
-				/>
-				<div className=" flex flex-col justify-between">
-					<img
-						src={other6_2}
+				>
+					<div>
+						<div className=" font-semibold">{t('luck2.miaosha1Text')}</div>
+						<div>{t('luck2.miaosha1Time')}</div>
+					</div>
+					<img src={pod7} className=" w-full mt-2" alt="" />
+				</div>
+				<div className="w-[48%] flex flex-col justify-between">
+					<div
+						className=" relative bg-gradient-to-br from-[#d5d5f9] to-[#fefeff] py-3 rounded-2xl px-2"
 						onClick={jumpApp}
-						className=" w-[168px] h-[113px]"
-						alt=""
-					/>
-					<img
-						src={other6_3}
+					>
+						<div className=" font-semibold">{t('luck2.miaosha2Text')}</div>
+						<div>{t('luck2.miaosha2Time')}</div>
+						<div className=" mt-4 leading-3">{t('luck2.miaosha2Desc')}</div>
+						<img
+							src={pod8}
+							className=" absolute w-[42%] right-2 bottom-2"
+							alt=""
+						/>
+					</div>
+					<div
+						className=" relative bg-gradient-to-br from-[#d5d5f9] to-[#fefeff] py-3 rounded-2xl px-2"
 						onClick={jumpApp}
-						className=" w-[168px] h-[113px]"
-						alt=""
-					/>
+					>
+						<div className=" font-semibold">{t('luck2.miaosha3Text')}</div>
+						<div>{t('luck2.miaosha3Time')}</div>
+						<div className=" mt-4 leading-3">{t('luck2.miaosha3Desc')}</div>
+						<img
+							src={pod9}
+							className=" absolute w-[42%] right-2 bottom-2"
+							alt=""
+						/>
+					</div>
 				</div>
 			</div>
 
 			<div className="px-4">
-				<div
-					className={classNames(
-						'h-[380px] w-full bg-cover pt-[50px] relative z-1 mt-6',
-						{
-							" bg-[url('/src/assets/taiImg/luck2/other7_bg.png')] ": [
-								'tai',
-							].includes(lang),
-						}
-					)}
-				>
+				<div className=" w-full bg-gradient-to-br from-[#f7e1d5] to-[#ffffff] py-1 pb-4 rounded-2xl mt-4 relative">
 					<img
-						src={Tag2}
+						src={otherTag}
 						className=" w-[88px] h-[95px] absolute right-1 -top-8"
 						alt=""
 					/>
+
+					<div className=" relative w-fit ml-4 mt-2 ">
+						<div className=" relative z-10">{t('luck2.renqiTitle')}</div>
+						<div className=" absolute w-full h-[4px] bg-[#cfb9f7] bottom-1"></div>
+					</div>
 
 					<div className=" w-[315px] h-[41px] flex justify-between mt-4 mx-auto">
 						<div className=" w-1/2 h-full flex justify-center items-center bg-[#fffcfa] rounded-3xl">
@@ -136,12 +267,28 @@ export function OtherProduct() {
 						</div>
 					</div>
 
-					<img
-						src={other7_1}
-						className=" w-[308px] h-[242px] mx-auto mt-4"
-						alt=""
-						onClick={jumpApp}
-					/>
+					<div className=" px-4 mt-4">
+						<div className=" w-full text-sm flex flex-col justify-center items-center bg-gradient-to-r from-[#f1e7f9] to-[#f8e3e7] py-4">
+							<div>{t('luck2.renqiComputer')}</div>
+							<img src={pod10} className=" w-[50%] mt-4" alt="" />
+						</div>
+						<div className=" w-full h-[55px] bg-[#e173a3] flex">
+							<div className=" w-1/2 rounded-br-3xl bg-[#fbedda] h-full flex items-center justify-center">
+								<div className=" text-xs mt-2">
+									{t('luck2.renqiNormalText')}{' '}
+								</div>
+								<div className=" text-2xl font-semibold">
+									{t('luck2.renqiNormalPrice')}
+								</div>
+							</div>
+							<div className=" w-1/2 flex items-center justify-center text-white">
+								<div className=" text-xs mt-2">{t('luck2.renqiNowText')}</div>
+								<div className=" text-2xl font-semibold">
+									{t('luck2.renqiNowPrice')}
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
